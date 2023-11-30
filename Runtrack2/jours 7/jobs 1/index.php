40bffd2e3296e2c05cc8d12c1d4168fc1e8cf72f
LordDragons <149957558+LordDragons@users.noreply.github.com>
2023-11-30 18:00:39 +0100

    php
    mise en ligne des exercices

---
+++ Runtrack2/jours 7/jobs 1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello LaPlateforme</title>
+</head>
+<body>
+
+<?php
+$a='Hello';
+$b='LaPlateforme';
+
+ function hello($a, $b) {
+    return $a.' '.$b;
+}
+echo hello($a, $b);
+?>
+
+</body>
+</html>
